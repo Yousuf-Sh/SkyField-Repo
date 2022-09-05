db8db07c1fe86b18a0cadfcf8f973018d535a0e3
Yousuf-Sh <ys937721@gmail.com>
2022-09-05 14:25:54 +0500

    Health System finalized
    The health system was re-implemented in order to attempt to un-borke the conflict being caused

---
+++ SkyField/Assets/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public int damageAmmount;
+    public healthBar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.setMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage(damageAmmount);
+        }
+
+    }
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.setHealth(currentHealth);
+    }
+}
